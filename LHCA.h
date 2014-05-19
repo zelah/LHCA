dec5513ed9f690b7000716f4fb2c33688f35ca09
zelah <zelah@inbox.com>
2014-05-18 23:12:30 -0700

    Create LHCA.h

---
+++ LHCA.h
@@ -0,0 +1,17 @@
+#include "MACRO.m"
+
+@interface LHCA : NSObject
+{ucharP state;}
+-(uchar) forward;
+-(uchar) backward;
+-(uchar) initialize;
+-(uchar) one;
+-(uchar) two;
+-(uchar) three;
+-(uchar) four;
+-(uchar) five;
+-(uchar) six;
+-(uchar) seven;
+-(uchar) eight;
+-(uchar) seed: (uchar) one : (uchar) two : (uchar) three : (uchar) four : (uchar) five : (uchar) six : (uchar) seven : (uchar) eight;
+@end
